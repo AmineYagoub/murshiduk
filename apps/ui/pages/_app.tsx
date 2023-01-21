29e5c8cdd585a1c3b273ed220c773c89896db8ad
Amine Yagoub <yagoub.2.amine@gmail.com>
2023-01-21 08:05:39 +0100

    feat(ui): init global style

--- apps/ui/pages/_app.tsx
+++ apps/ui/pages/_app.tsx
@@ -1,15 +1,14 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
 import 'antd/dist/reset.css';
-
+import './global.css';
 import ar from 'antd/lib/locale/ar_EG';
 import { NextComponentType } from 'next';
 import { ReactElement, useEffect } from 'react';
 import { LoadingOutlined } from '@ant-design/icons';
 import { ConfigProvider, notification, Spin } from 'antd';
 import { CacheProvider, EmotionCache } from '@emotion/react';
-import CreateEmotionCache from '../config/CreateEmotionCache';
-import theme from '../config/Theme';
+import CreateEmotionCache from '@/config/CreateEmotionCache';
+import theme from '@/config/Theme';
 
 const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 Spin.setDefaultIndicator(<Spin indicator={antIcon} />);
@@ -37,7 +36,7 @@ export default function CustomApp(props: MyAppProps) {
   }, []);
   return (
     <CacheProvider value={emotionCache}>
-      <ConfigProvider locale={ar} direction='rtl' theme={theme}>
+      <ConfigProvider locale={ar} direction="rtl" theme={theme}>
         <Component {...pageProps} />
       </ConfigProvider>
     </CacheProvider>
