3320b127e0ca3d589cad403e739ea04c3ff313a5
Amine Yagoub <yagoub.2.amine@gmail.com>
2023-01-28 12:26:18 +0100

    style(ui): chnage colorBgBase color

--- apps/ui/config/Theme.ts
+++ apps/ui/config/Theme.ts
@@ -3,6 +3,7 @@ import { ThemeConfig } from 'antd/es/config-provider/context';
 const theme: ThemeConfig = {
   token: {
     colorPrimary: '#00b96b',
+    colorBgBase: '#283a2c',
     fontSize: 16,
     borderRadius: 16,
     wireframe: true,
