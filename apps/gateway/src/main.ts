8cf50ff958a3179ed7b0918a5e745e292aa2fc70
Amine Yagoub <yagoub.2.amine@gmail.com>
2023-01-30 18:48:27 +0100

    feat(api): add listener for Prisma beforeExit event

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -5,6 +5,7 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app/app.module';
+import { PrismaService } from './app/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,6 +14,8 @@ async function bootstrap() {
   );
   const port = 3000;
   await app.listen(port, '0.0.0.0');
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
