d2d20ce45184b4c4650c59d886ec2dfac48414f7
Amine Yagoub <yagoub.2.amine@gmail.com>
2023-02-18 08:53:40 +0100

    fix(ui): debug cookie in nextjs middleware

--- apps/ui/middleware.ts
+++ apps/ui/middleware.ts
@@ -7,12 +7,14 @@ import type { NextRequest } from 'next/server';
 import { AppRoutes } from './utils';
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
+  console.log(request.cookies.getAll());
   const cookie = request.cookies.get(appConfig.NONCE_NAME);
-  if (!cookie) {
+  console.log(cookie, appConfig.NONCE_NAME);
+  /*   if (!cookie) {
     const url = request.nextUrl.clone();
     url.pathname = AppRoutes.SignIn;
     return NextResponse.redirect(url);
-  }
+  } */
 
   return response;
 }
