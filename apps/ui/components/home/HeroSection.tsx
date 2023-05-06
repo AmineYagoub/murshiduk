4224fb22dee77dfadf8d6f5dcd4ed0581e7e7eb2
Amine Yagoub <yagoub.2.amine@gmail.com>
2023-05-06 08:51:51 +0100

    fix(ui): load carousel images conditionally for each device

--- apps/ui/components/home/HeroSection.tsx
+++ apps/ui/components/home/HeroSection.tsx
@@ -3,12 +3,14 @@ import { Carousel } from 'antd';
 import styled from '@emotion/styled';
 import { baseS3Url } from '@/utils/index';
 import { CarouselEl } from '@/utils/types';
+import Image from 'next/image';
+import isMobile from 'is-mobile';
 
 const StyledCarousel = styled(Carousel)({
   width: '100%',
   height: '100vh',
   position: 'relative',
-  marginTop: 65,
+  marginTop: isMobile() ? 50 : 30,
   img: {
     height: '100vh !important',
     objectFit: 'fill',
@@ -23,12 +25,14 @@ const HeroSection = ({ images }: { images: CarouselEl[] }) => {
   return (
     <StyledCarousel autoplay fade>
       {images.map((el, i) => (
-        <img
+        <Image
+          fill
           key={i}
-          src={`${baseS3Url}/${el.lg}`}
+          src={isMobile() ? `${baseS3Url}/${el.sm}` : `${baseS3Url}/${el.lg}`}
+          loader={() =>
+            isMobile() ? `${baseS3Url}/${el.sm}` : `${baseS3Url}/${el.lg}`
+          }
           alt="مرشد سياحي في تركيا"
-          srcSet={`${baseS3Url}/${el.sm} 480w, ${baseS3Url}/${el.md} 800w`}
-          sizes="(max-width: 600px) 480px, 100vw"
         />
       ))}
     </StyledCarousel>
